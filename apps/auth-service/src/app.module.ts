04a775fd02b46778814fda40501eb1d2530ad5a7
Felipe Batista <felipe.batistaa52@gmail.cpm>
2025-03-17 06:19:49 -0300

    fix(postgres): configure postgres db
    * Includes the .env import
    * Configures TypeOrm

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -3,15 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [AuthModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
       port: parseInt(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
+      password: String(process.env.POSTGRES_PASSWORD),
       database: process.env.POSTGRES_DB,
       entities: [],
       synchronize: true,
