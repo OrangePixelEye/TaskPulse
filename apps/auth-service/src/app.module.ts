bcce54c5ffe8726e16be71047885e69aa087f142
Felipe Batista <felipe.batistaa52@gmail.cpm>
2025-03-17 06:38:17 -0300

    revert(TypeORM): auto entity import

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -4,21 +4,20 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { Auth } from './auth/entities/auth.entity';
 
 @Module({
   imports: [AuthModule,
     ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        host: process.env.POSTGRES_HOST,
-        port: parseInt(process.env.POSTGRES_PORT),
-        username: process.env.POSTGRES_USER,
-        password: String(process.env.POSTGRES_PASSWORD),
-        database: process.env.POSTGRES_DB,
-        entities: [__dirname + '/../**/*.entity.{ts,js}'],
-        synchronize: true,
-      })
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: String(process.env.POSTGRES_PASSWORD),
+      database: process.env.POSTGRES_DB,
+      entities: [Auth],
+      synchronize: true,
     })
   ],
   controllers: [AppController],
