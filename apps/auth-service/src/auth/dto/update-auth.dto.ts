d6e76479bc90c3a417f94ebb9c174ed5086fe6ac
Felipe Batista <felipe.batistaa52@gmail.cpm>
2025-03-17 06:17:36 -0300

    feat(auth): Implements class validator
    * Adds validation for create and update dto

--- apps/auth-service/src/auth/dto/update-auth.dto.ts
+++ apps/auth-service/src/auth/dto/update-auth.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAuthDto } from './create-auth.dto';
+import { IsOptional, IsString, MinLength } from 'class-validator';
 
-export class UpdateAuthDto extends PartialType(CreateAuthDto) {}
+export class UpdateAuthDto extends PartialType(CreateAuthDto) {
+  @IsString()
+  @IsOptional()
+  @MinLength(8)
+  password?: string;
+}
